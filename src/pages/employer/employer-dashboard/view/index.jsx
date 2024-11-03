c87ef0b98e7a2b54caa562030648ed255d55c090
Sandaris <148203044+Sandaris@users.noreply.github.com>
2024-11-03 23:03:51 +0800

    simple employer dashboard

--- src/pages/employer/employer-dashboard/view/index.jsx
+++ src/pages/employer/employer-dashboard/view/index.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import "./style.css";
 import { Text } from "@radix-ui/themes";
+import Dashboard from "./components/dashboard";
 
 const EmployerDashboard = () => {
-    return <Text>Dashboard</Text>;
+    return (
+        <div>
+            <Text>Employer Dashboard</Text>
+            <Dashboard /> {/* Render the Dashboard component here */}
+        </div>
+    );
 };
 
 export default EmployerDashboard;
+
