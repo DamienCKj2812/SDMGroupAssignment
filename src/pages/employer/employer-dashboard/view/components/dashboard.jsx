2ca824d777e7f856aab497f03d888168e52b7e19
Sandaris <148203044+Sandaris@users.noreply.github.com>
2024-11-05 23:58:43 +0800

    added line chart and pie chart

--- src/pages/employer/employer-dashboard/view/components/dashboard.jsx
+++ src/pages/employer/employer-dashboard/view/components/dashboard.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import '../style.css';
+import { useNavigate } from 'react-router-dom';
+import {Flex, ScrollArea} from "@radix-ui/themes";
 
 const Dashboard = () => {
+
+    const navigate = useNavigate()
   return (
     <div className="container">
       <header className="header">
@@ -22,51 +26,39 @@ const Dashboard = () => {
 
                 <h1 className="title">Find the best person for your role</h1>
 
-        <div className="cards">
+
+        <Flex className="panel1">
+        <Flex className="cards" direction="column">
           <div className="card">
             <h2>Create a job ad</h2>
-            <p>Create a job ad that will attract the best candidates for your role.</p>
+            
+            <p>Create a job ad that will attract the best candidates.</p>
             <svg className="icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
               <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6"></path>
             </svg>
-            <button className="card-button">Create job ad</button>
+            <button className="card-button" onClick={() => navigate(`../employer/create-job-ad`)}>Create job ad</button>
           </div>
 
-                    <div className="card">
-                        <h2>Choose your ad type</h2>
-                        <p>Choose the ad type that best suits your needs.</p>
-                        <svg className="icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-                            ></path>
-                        </svg>
-                        <button className="card-button">Choose ad type</button>
-                    </div>
+        <div className="card">
+            <h2>Manage your candidates</h2>
+            <p>Manage your candidates and track their progress.</p>
+            <svg className="icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2"
+                ></path>
+            </svg>
+            <button className="card-button">Manage candidates</button>
+            </div>
+        </Flex>
 
-                    <div className="card">
-                        <h2>Manage your candidates</h2>
-                        <p>Manage your candidates and track their progress.</p>
-                        <svg className="icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2"
-                            ></path>
-                        </svg>
-                        <button className="card-button">Manage candidates</button>
-                    </div>
-                </div>
-
+            <Flex direction="column">
                 <h2 className="title" style={{ fontSize: "1.5rem", marginTop: "2rem" }}>
                     My recent job ads
                 </h2>
-
-                <table className="table">
-                    <thead>
+                <table className="table"><thead>
                         <tr>
                             <th>Job title</th>
                             <th>Status</th>
@@ -76,6 +68,10 @@ const Dashboard = () => {
                             <th>Job actions</th>
                         </tr>
                     </thead>
+                    </table>
+                
+                <ScrollArea type="always" scrollbars="vertical" style={{ height: '50%', overflowY: 'auto' }}>
+                <table className="table">
                     <tbody>
                         <tr>
                             <td>Software Engineer</td>
@@ -99,8 +95,67 @@ const Dashboard = () => {
                                 <button>Delete</button>
                             </td>
                         </tr>
+                        <tr>
+                            <td>UX Designer</td>
+                            <td>Inactive</td>
+                            <td>5</td>
+                            <td>Disabled</td>
+                            <td>Low</td>
+                            <td className="table-actions">
+                                <button>Edit</button>
+                                <button>Delete</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>DevOps Engineer</td>
+                            <td>Inactive</td>
+                            <td>5</td>
+                            <td>Disabled</td>
+                            <td>Low</td>
+                            <td className="table-actions">
+                                <button>Edit</button>
+                                <button>Delete</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Full Stack Developer</td>
+                            <td>Inactive</td>
+                            <td>5</td>
+                            <td>Disabled</td>
+                            <td>Low</td>
+                            <td className="table-actions">
+                                <button>Edit</button>
+                                <button>Delete</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Backend Developer</td>
+                            <td>Inactive</td>
+                            <td>5</td>
+                            <td>Disabled</td>
+                            <td>Low</td>
+                            <td className="table-actions">
+                                <button>Edit</button>
+                                <button>Delete</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>Frontend Developer</td>
+                            <td>Inactive</td>
+                            <td>5</td>
+                            <td>Disabled</td>
+                            <td>Low</td>
+                            <td className="table-actions">
+                                <button>Edit</button>
+                                <button>Delete</button>
+                            </td>
+                        </tr>
+                        
                     </tbody>
                 </table>
+                </ScrollArea>
+                </Flex>
+                </Flex>
             </div>
         </div>
     );
